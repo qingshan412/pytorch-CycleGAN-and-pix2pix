58320c3bd68fe8806e2b89618f537dda1cbae286
qingshan412 <qingshan412@hotmail.com>
2018-09-14 14:44:33 -0700

    test

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
-rm -r ./checkpoints/maps_cyclegan
-python train.py --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan --display_id -1 --no_html > rec/maps_cyclegan_rec
+# rm -r ./checkpoints/maps_cyclegan
+# python train.py --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan --display_id -1 --no_html > rec/maps_cyclegan_rec
+
+python train.py --dataroot ./datasetss/maps --name maps_cyclegan_batch20 --model cycle_gan --batch_size 20 --niter 5 --niter_decay 5 --display_id -1 --no_html > rec/maps_cyclegan_batch20_rec
+
+# python train.py --dataroot ./datasetss/maps --name maps_cyclegan_batch20_iter10 --model cycle_gan --batch_size 20 --niter 10 --niter_decay 10 --display_id -1 --no_html > rec/maps_cyclegan_batch20_iter10_rec
