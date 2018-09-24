35602a6ea193f67ea52eb4c947559ad561036869
qingshan412 <qingshan412@hotmail.com>
2018-09-24 15:24:28 -0700

    CTreading

--- options/base_options.py
+++ options/base_options.py
@@ -16,8 +16,8 @@ class BaseOptions():
         parser.add_argument('--loadSize', type=int, default=286, help='scale images to this size')
         parser.add_argument('--fineSize', type=int, default=256, help='then crop to this size')
         parser.add_argument('--display_winsize', type=int, default=256, help='display window size') ### J.L.
-        parser.add_argument('--input_nc', type=int, default=3, help='# of input image channels')
-        parser.add_argument('--output_nc', type=int, default=3, help='# of output image channels')
+        parser.add_argument('--input_nc', type=int, default=1, help='# of input image channels')
+        parser.add_argument('--output_nc', type=int, default=1, help='# of output image channels')
         parser.add_argument('--ngf', type=int, default=64, help='# of gen filters in first conv layer')
         parser.add_argument('--ndf', type=int, default=64, help='# of discrim filters in first conv layer')
         parser.add_argument('--netD', type=str, default='basic', help='selects model to use for netD')
@@ -25,7 +25,7 @@ class BaseOptions():
         parser.add_argument('--n_layers_D', type=int, default=3, help='only used if netD==n_layers')
         parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
         parser.add_argument('--name', type=str, default='experiment_name', help='name of the experiment. It decides where to store samples and models')
-        parser.add_argument('--dataset_mode', type=str, default='unaligned', help='chooses how datasets are loaded. [unaligned | aligned | single]')
+        parser.add_argument('--dataset_mode', type=str, default='unalignedCT', help='chooses how datasets are loaded. [unaligned | aligned | single | unalignedCT]')
         parser.add_argument('--model', type=str, default='cycle_gan',
                             help='chooses which model to use. cycle_gan, pix2pix, test')
         parser.add_argument('--direction', type=str, default='AtoB', help='AtoB or BtoA')
