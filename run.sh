a4b7f19d8705e6d6919d23138e9caad461d2045f
qingshan412 <qingshan412@hotmail.com>
2018-09-26 17:20:37 -0700

    ctest

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 # python train.py --dataroot ./datasets/ctest --name ctest_cyclegan_batch4 --model cycle_gan --batch_size 4 --niter 25 --niter_decay 25 --display_id -1 --no_html > rec/ctest_cyclegan_batch4_rec
 # python train_ct.py --dataroot ./datasets/ctest --name ctest_cyclegan_batch4 --model cycle_gan --batch_size 4 --niter 25 --niter_decay 25 --display_id -1 > rec/ctest_cyclegan_batch4_html_rec
 # python train_ct.py --dataroot ./datasets/ctest --name ctest_cyclegan_batch4_epoch50 --model cycle_gan --batch_size 4 --niter 50 --niter_decay 50 --display_id -1 > rec/ctest_cyclegan_batch4_epoch50_html_rec
-# python train_ct.py --dataroot ./datasets/ctest --name ctest_cyclegan_batch4_epoch50_idt0 --model cycle_gan --batch_size 4 --niter 50 --niter_decay 50 --display_id -1 --lambda_identity 0 > rec/ctest_cyclegan_batch4_epoch50_idt0_html_rec
+python train_ct.py --dataroot ./datasets/ctest --name ctest_cyclegan_batch4_epoch50_idt0 --model cycle_gan --batch_size 4 --niter 50 --niter_decay 50 --display_id -1 --gpu_ids 1 --lambda_identity 0 > rec/ctest_cyclegan_batch4_epoch50_idt0_html_rec
 
 ### low
-python train_ct.py --dataroot ./datasets/ctestL --name ctestL_cyclegan_batch4 --model cycle_gan --batch_size 4 --niter 25 --niter_decay 25 --display_id -1 > rec/ctestL_cyclegan_batch4_html_rec
+# python train_ct.py --dataroot ./datasets/ctestL --name ctestL_cyclegan_batch4 --model cycle_gan --batch_size 4 --niter 25 --niter_decay 25 --display_id -1 > rec/ctestL_cyclegan_batch4_html_rec
 
 ### batch2
 # python train.py --dataroot ./datasets/ctest --name ctest_cyclegan_batch2 --model cycle_gan --batch_size 2 --niter 50 --niter_decay 50 --display_id -1 --no_html > rec/ctest_cyclegan_batch2_rec
