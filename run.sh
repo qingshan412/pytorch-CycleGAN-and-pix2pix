2a29ae4916d8f6f9da44dbbc34056a1433189bc6
qingshan412 <qingshan412@hotmail.com>
2018-10-22 15:25:26 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,11 @@
 # python train_ct.py --dataroot ./datasets/tw_np --name twnp_cyclegan_batch4 --model cycle_gan --batch_size 4 --niter 25 --niter_decay 25 --display_id -1 > rec/twnp_cyclegan_batch4_html_rec
 
 ### all tw dataset
-python train_ct.py --dataroot ./datasets/tw_np_all --name twnpa_cyclegan_batch4 --model cycle_gan --batch_size 4 --niter 25 --niter_decay 25 --display_id -1 --gpu_ids 1 > rec/twnpa_cyclegan_batch4_html_rec
+# python train_ct.py --dataroot ./datasets/tw_np_all --name twnpa_cyclegan_batch4 --model cycle_gan --batch_size 4 --niter 25 --niter_decay 25 --display_id -1 --gpu_ids 1 > rec/twnpa_cyclegan_batch4_html_rec
 # python test_ct_1.py --dataroot ./datasets/tw_np_all --name twnpa_cyclegan_batch4 --model cycle_gan
+
+### one tw dataset
+python train_ct.py --dataroot ./datasets/tw_np_one --name twnpo_cyclegan_batch4 --model cycle_gan --batch_size 4 --niter 25 --niter_decay 25 --display_id -1 --gpu_ids 1 > rec/twnpo_cyclegan_batch4_html_rec
 
 #####################################dcm
 ### batch4
