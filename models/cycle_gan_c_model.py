d67a0dbc2a237c92cbcde11162dbf63193514afd
qingshan412 <qingshan412@hotmail.com>
2018-10-31 10:35:53 -0700

    Update models/cycle_gan_c_model.py

--- models/cycle_gan_c_model.py
+++ models/cycle_gan_c_model.py
@@ -5,9 +5,9 @@ from .base_model import BaseModel
 from . import networks
 
 
-class CycleGANModel(BaseModel):
+class CycleGANcModel(BaseModel):
     def name(self):
-        return 'CycleGANModel'
+        return 'CycleGANcModel'
 
     @staticmethod
     def modify_commandline_options(parser, is_train=True):
