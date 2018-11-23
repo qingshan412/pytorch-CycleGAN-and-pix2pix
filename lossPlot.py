90ab02d5787f60583a99ff72bcbe52b21e486b5f
qingshan412 <qingshan412@hotmail.com>
2018-11-23 15:57:36 -0800

    Update lossPlot.py

--- lossPlot.py
+++ lossPlot.py
@@ -36,17 +36,23 @@ while 1:
             if flag:
                 i = 8
                 while i < len(line):
-                    if line[i][0].upper == 'D' or line[i][0].upper == 'G':
-                        if 'cycleganc' in args.SourceDir:
-                            name_i = line[i][0].upper() + line[i][1:-1].replace('A', 'X').replace('B', 'Y').replace('C', 'Z')
-                        elif 'decouple_cb200' in args.SourceDir:
-                            name_i = line[i][0].upper() + line[i][1:-1].replace('A', 'Z').replace('B', 'Y')
-                        elif 'decouple_ac200' in args.SourceDir:
+                    if 'decouple_ac200' in args.SourceDir:
+                        if line[i][0].upper == 'D' or line[i][0].upper == 'G':
                             name_i = line[i][0].upper() + line[i][1:-1].replace('A', 'Z').replace('B', 'X')
                         else:
-                            name_i = line[i][0].upper() + line[i][1:-1].replace('A', 'Y').replace('B', 'X')
-                    else:
+                            name_i = line[i][0].upper() + line[i][1:-1].replace('A', 'X').replace('B', 'Z')
+                    elif 'decouple_cb200' in args.SourceDir:
+                        if line[i][0].upper == 'D' or line[i][0].upper == 'G':
+                            name_i = line[i][0].upper() + line[i][1:-1].replace('A', 'Y').replace('B', 'Z')
+                        else:
+                            name_i = line[i][0].upper() + line[i][1:-1].replace('A', 'Z').replace('B', 'Y')
+                    elif 'cycleganc' in args.SourceDir:
                         name_i = line[i][0].upper() + line[i][1:-1].replace('A', 'X').replace('B', 'Y').replace('C', 'Z')
+                    else:
+                        if line[i][0].upper == 'D' or line[i][0].upper == 'G':
+                            name_i = line[i][0].upper() + line[i][1:-1].replace('A', 'Y').replace('B', 'X').replace('C', 'Z')
+                        else:
+                            name_i = line[i][0].upper() + line[i][1:-1].replace('A', 'X').replace('B', 'Y').replace('C', 'Z')
                     idx_name.append(name_i)
                     losses[name_i] = []
                     i += 2
