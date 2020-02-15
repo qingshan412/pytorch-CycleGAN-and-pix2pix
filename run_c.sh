1c398df385fe1f3c68ec434d67d96d6d8335a0ea
qingshan412 <qingshan412@hotmail.com>
2020-02-14 22:52:19 -0500

    Update run_c.sh

--- run_c.sh
+++ run_c.sh
@@ -20,8 +20,6 @@ ModelName=pix2pix #cycle_gan
 python train_fr_aligned.py \
   --dataroot ../InsightFace_Pytorch/data/facebank/noonan+normal \
   --continue_train \
-  --display_freq \
-  --update_html_freq \
   --name fr_aligned_basic_b4_${ModelName}_b${BatchSize} --dataset_mode unaligned --model $ModelName \
   --netG resnet_4blocks --batch_size $BatchSize --niter 100 --niter_decay 100 \
   --display_id -1 --gpu_ids $CUDA_VISIBLE_DEVICES --serial_batches > rec/fr_aligned_${ModelName}_4b_b${BatchSize}_html_serial_con_rec 
