ca2ec5e8ff7408f2e4f54edb1d5a1fa1d606dc41
qingshan412 <qingshan412@hotmail.com>
2020-03-03 16:01:02 -0500

    Update run_c.sh

--- run_c.sh
+++ run_c.sh
@@ -22,7 +22,8 @@ python train_fr_aligned.py \
   --dataroot ../InsightFace_Pytorch/data/facebank/noonan+normal \
   --continue_train \
   --name fr_aligned_basic_b4_${ModelName}_b${BatchSize} --dataset_mode unaligned --model $ModelName \
-  --netG resnet_4blocks --batch_size $BatchSize --niter 1 --niter_decay 1 \
+  --netG resnet_4blocks --batch_size $BatchSize \
+  --niter 1 --niter_decay 1 --pool_size 5\
   --display_id -1 --gpu_ids $CUDA_VISIBLE_DEVICES --serial_batches > rec/fr_train_test_rec 
 #################### test on changed landmarks
 # python test_fr_aligned.py \
