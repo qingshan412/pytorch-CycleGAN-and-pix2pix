d1d05cef47ae4918d67434e8a01fb84c24bb46ef
qingshan412 <qingshan412@hotmail.com>
2020-03-09 23:28:48 -0400

    Update pix2pix_transfer_model.py

--- models/pix2pix_transfer_model.py
+++ models/pix2pix_transfer_model.py
@@ -31,7 +31,7 @@ class Pix2PixTransferModel(BaseModel):
         BaseModel.initialize(self, opt)
         self.isTrain = opt.isTrain
         # specify the training losses you want to print out. The program will call base_model.get_current_losses
-        self.loss_names = ['G_GAN', 'G_L1', 'D_real', 'D_fake']
+        self.loss_names = ['G_GAN', 'G_L1'] #, 'D_real', 'D_fake']
         # specify the images you want to save/display. The program will call base_model.get_current_visuals
         self.visual_names = ['real_A', 'fake_B', 'real_B']
         # specify the models you want to save to the disk. The program will call base_model.save_networks and base_model.load_networks
