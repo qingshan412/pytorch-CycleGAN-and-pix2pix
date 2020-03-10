6decdae00d3a99592faf8767205a7d5c89562dcc
qingshan412 <qingshan412@hotmail.com>
2020-03-10 17:27:46 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,15 +11,24 @@ module load python pytorch        # Required modules
 
 BatchSize=6
 ModelName=pix2pix #cycle_gan
-#################### test train 
-python train_fr_aligned.py \
-  --dataroot ../InsightFace_Pytorch/data/facebank/noonan+normal \
-  --continue_train \
-  --name fr_aligned_basic_b4_${ModelName}_b${BatchSize} --dataset_mode unaligned --model $ModelName \
-  --netG resnet_4blocks --batch_size $BatchSize \
-  --niter 1 --niter_decay 1 --pool_size 5\
-  --display_id -1 --gpu_ids $CUDA_VISIBLE_DEVICES --serial_batches > rec/fr_train_test_rec_2 
 
+#################### test
+python test_fr_aligned.py \
+  --dataroot ../InsightFace_Pytorch/data/facebank/noonan+normal \
+  --name fr_adult_${ModelName}_b${BatchSize} \
+  --dataset_mode unaligned \
+  --model $ModelName \
+  --netG resnet_4blocks \
+  --num_test 100 \
+  --gpu_ids $CUDA_VISIBLE_DEVICES > rec/Dtest 
+#################### test train 
+# python train_fr_aligned.py \
+#   --dataroot ../InsightFace_Pytorch/data/facebank/noonan+normal \
+#   --continue_train \
+#   --name fr_aligned_basic_b4_${ModelName}_b${BatchSize} --dataset_mode unaligned --model $ModelName \
+#   --netG resnet_4blocks --batch_size $BatchSize \
+#   --niter 1 --niter_decay 1 --pool_size 5\
+#   --display_id -1 --gpu_ids $CUDA_VISIBLE_DEVICES --serial_batches > rec/fr_train_test_rec_2 
 
 # # mkdir -p /tmp/jliu16
 # # rsync -a ~/Private/Research/2020/FR/InsightFace_Pytorch/data/facebank/webface /tmp/jliu16/$JOB_ID
