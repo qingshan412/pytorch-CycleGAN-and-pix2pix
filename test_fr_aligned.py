671bfe7812ef40aec9f4f639d69f230e049e5887
qingshan412 <qingshan412@hotmail.com>
2020-03-09 22:07:37 -0400

    Update test_fr_aligned.py

--- test_fr_aligned.py
+++ test_fr_aligned.py
@@ -4,6 +4,7 @@ from data import CreateDataLoader
 from models import create_model
 from util.visualizer import save_images
 from util import html
+from models.pix2pix_transfer_model import filtered_params
 
 
 if __name__ == '__main__':
@@ -17,9 +18,7 @@ if __name__ == '__main__':
     dataset = data_loader.load_data()
     model = create_model(opt)
     model.setup(opt)
-    print(model.netG.named_parameters())
-    for name, param in model.netG.named_parameters():
-        print(name)
+    print(filtered_params(model.netG))
     exit(0)
     # create website
     web_dir = os.path.join(opt.results_dir, opt.name, '%s_%s' % (opt.phase, opt.epoch)) ### ./results/maps_cyclegan/test_[epoch]
