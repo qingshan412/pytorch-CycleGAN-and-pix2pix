ca65bd2afc9d161723c2679bc97bb91f1c841db1
qingshan412 <qingshan412@hotmail.com>
2020-03-11 18:31:16 -0400

    Update run_fe.sh

--- run_fe.sh
+++ run_fe.sh
@@ -5,7 +5,7 @@ ModelName=pix2pix_transfer #pix2pix #cycle_gan
 Iter=2000 #25, 100, 500, 2000
 FolderName=fr_adult_${ModelName}_b${BatchSize}_${Iter}_1layer_pool5
 
-cp -r ./checkpoints/fr_adult_basic_b6 ./checkpoints/${FolderName}
+# cp -r ./checkpoints/fr_adult_basic_b6 ./checkpoints/${FolderName}
 #################### train on adults faces and then children faces
 python train_fr_aligned.py \
   --dataroot ../InsightFace_Pytorch/data/facebank/noonan+normal \
@@ -13,6 +13,6 @@ python train_fr_aligned.py \
   --pool_size 5\
   --name ${FolderName} \
   --dataset_mode unaligned --model $ModelName --netG resnet_4blocks \
-  --batch_size $BatchSize --niter $Epoch --niter_decay $Epoch \
+  --batch_size $BatchSize --niter $Iter --niter_decay $Iter \
   --display_id -1 --gpu_ids 1,2 \
   --serial_batches > rec/${FolderName}_fe_rec
