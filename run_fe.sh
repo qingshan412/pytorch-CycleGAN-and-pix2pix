5d971490d603a0fb3b4ac9e898662c92cd59ff40
qingshan412 <qingshan412@hotmail.com>
2020-03-11 00:08:20 -0400

    Update run_fe.sh

--- run_fe.sh
+++ run_fe.sh
@@ -8,7 +8,7 @@ Epoch=25 #25, 100, 500, 2000
 python train_fr_aligned.py \
   --dataroot ../InsightFace_Pytorch/data/facebank/noonan+normal \
   --continue_train \
-  --name fr_adult_${ModelName}_b${BatchSize}_${Epoch}_1layer \
+  --name fr_adult_${ModelName}_b${BatchSize}_${Epoch}_1layer_fe \
   --dataset_mode unaligned --model $ModelName --netG resnet_4blocks \
   --batch_size $BatchSize --niter $Epoch --niter_decay $Epoch \
   --display_id -1 --gpu_ids -1 \
