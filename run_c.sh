61bcf1f355b2ad395dee0198304ce82afb564e74
qingshan412 <qingshan412@hotmail.com>
2020-03-12 22:45:18 -0400

    Update run_c.sh

--- run_c.sh
+++ run_c.sh
@@ -17,7 +17,7 @@ module load python pytorch        # Required modules
 BatchSize=6
 ModelName=pix2pix_transfer #pix2pix #cycle_gan
 Epoch=500 #25, 100, 500, 2000
-FolderName=fr_adult_${ModelName}_b${BatchSize}_${Epoch}_1layer_pool5_DG_raw
+FolderName=fr_adult_${ModelName}_b${BatchSize}_${Epoch}_1layer_pool50_DG_raw
 
 [ -d "./checkpoints/${FolderName}" ] && rm -r ./checkpoints/${FolderName}
 cp -r ./checkpoints/fr_adult_basic_b6 ./checkpoints/${FolderName}
@@ -25,7 +25,7 @@ cp -r ./checkpoints/fr_adult_basic_b6 ./checkpoints/${FolderName}
 python train_fr_aligned.py \
   --dataroot ../InsightFace_Pytorch/data/facebank/noonan+normal \
   --continue_train \
-  --pool_size 5\
+  --pool_size 50\
   --name ${FolderName} \
   --dataset_mode unaligned --model $ModelName --netG resnet_4blocks \
   --batch_size $BatchSize --niter $Epoch --niter_decay $Epoch \
