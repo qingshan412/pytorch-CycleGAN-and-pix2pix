aeb64f06f94baf85506d4175ce87b23031506b2f
qingshan412 <qingshan412@hotmail.com>
2020-03-12 21:22:00 -0400

    Update run_c.sh

--- run_c.sh
+++ run_c.sh
@@ -17,7 +17,7 @@ module load python pytorch        # Required modules
 BatchSize=6
 ModelName=pix2pix_transfer #pix2pix #cycle_gan
 Epoch=500 #25, 100, 500, 2000
-FolderName=fr_adult_${ModelName}_b${BatchSize}_${Epoch}_1layer_pool5_DG
+FolderName=fr_adult_${ModelName}_b${BatchSize}_${Epoch}_1layer_pool5_DG_raw
 
 [ -d "./checkpoints/${FolderName}" ] && rm -r ./checkpoints/${FolderName}
 cp -r ./checkpoints/fr_adult_basic_b6 ./checkpoints/${FolderName}
