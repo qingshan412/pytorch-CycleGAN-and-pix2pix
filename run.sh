b0e23edbbcfcc3da3997011bfa7319a513717497
qingshan412 <qingshan412@hotmail.com>
2020-03-13 17:14:41 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -16,14 +16,14 @@ module load python pytorch        # Required modules
 # echo $CUDA_VISIBLE_DEVICES
 BatchSize=6
 ModelName=pix2pix_transfer #pix2pix #cycle_gan
-Epoch=500 #25, 100, 500, 2000
-FolderName=fr_adult_${ModelName}_b${BatchSize}_${Epoch}_1layer_pool5_DG_mix_raw
+Epoch=100 #25, 100, 500, 2000
+FolderName=fr_adult_${ModelName}_b${BatchSize}_${Epoch}_1layer_pool5_DG_rr
 
 [ -d "./checkpoints/${FolderName}" ] && rm -r ./checkpoints/${FolderName}
 cp -r ./checkpoints/fr_adult_basic_b6 ./checkpoints/${FolderName}
 #################### train on adults faces and then children faces
 python train_fr_aligned.py \
-  --dataroot ../InsightFace_Pytorch/data/facebank/webface+children \
+  --dataroot ../InsightFace_Pytorch/data/facebank/resize+raw \
   --continue_train \
   --pool_size 5\
   --name ${FolderName} \
