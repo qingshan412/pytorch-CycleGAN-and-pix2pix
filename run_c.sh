41d13286bfd31bf595be0ab578e14d5e43c5bab9
qingshan412 <qingshan412@hotmail.com>
2020-03-13 17:37:57 -0400

    Update run_c.sh

--- run_c.sh
+++ run_c.sh
@@ -16,7 +16,7 @@ module load python pytorch        # Required modules
 # echo $CUDA_VISIBLE_DEVICES
 BatchSize=6
 ModelName=pix2pix_transfer #pix2pix #cycle_gan
-Epoch=500 #25, 100, 500, 2000
+Epoch=100 #25, 100, 500, 2000
 FolderName=fr_adult_${ModelName}_b${BatchSize}_${Epoch}_1layer_pool50_DG_raw
 
 [ -d "./checkpoints/${FolderName}" ] && rm -r ./checkpoints/${FolderName}
@@ -30,6 +30,24 @@ python train_fr_aligned.py \
   --batch_size $BatchSize --niter $Epoch --niter_decay $Epoch \
   --display_id -1 --gpu_ids $CUDA_VISIBLE_DEVICES \
   --serial_batches > rec/${FolderName}_rec 
+
+
+
+
+FolderName=fr_adult_${ModelName}_b${BatchSize}_${Epoch}_1layer_pool5_DG_rr
+[ -d "./checkpoints/${FolderName}" ] && rm -r ./checkpoints/${FolderName}
+cp -r ./checkpoints/fr_adult_basic_b6 ./checkpoints/${FolderName}
+#################### train on adults faces and then children faces
+python train_fr_aligned.py \
+  --dataroot ../InsightFace_Pytorch/data/facebank/resize+raw \
+  --continue_train \
+  --pool_size 5\
+  --name ${FolderName} \
+  --dataset_mode unaligned --model $ModelName --netG resnet_4blocks \
+  --batch_size $BatchSize --niter $Epoch --niter_decay $Epoch \
+  --display_id -1 --gpu_ids $CUDA_VISIBLE_DEVICES \
+  --serial_batches > rec/${FolderName}_rec 
+  
 #################### test train 
 # python train_fr_aligned.py \
 #   --dataroot ../InsightFace_Pytorch/data/facebank/noonan+normal \
