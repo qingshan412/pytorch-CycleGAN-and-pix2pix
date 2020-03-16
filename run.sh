5f7b814a2bed19d01b59b7f6aa84ce1cf4c9415c
qingshan412 <qingshan412@hotmail.com>
2020-03-15 22:28:41 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -17,7 +17,7 @@ module load python pytorch        # Required modules
 
 # BatchSize=6
 ModelName=pix2pix_transfer #pix2pix #cycle_gan
-Epoch=20
+# Epoch=20
 # Iter=100 #25, 100, 500, 2000
 # FolderName=fr_adult_${ModelName}_b${BatchSize}_${Iter}_1layer_pool5_DG_rr
 #################### train on adults faces and then children faces
@@ -34,7 +34,7 @@ Epoch=20
 #   --serial_batches > rec/${FolderName}_rec 
 
 #################### test
-FolderName=fr_adult_pix2pix_transfer_b6_25_2layer
+FolderName=fr_aug_pix2pix_transfer_b6_100_2layer
 python test_fr_aligned.py \
   --dataroot ../InsightFace_Pytorch/data/facebank/noonan+normal/ \
   --name $FolderName \
@@ -44,25 +44,35 @@ python test_fr_aligned.py \
   --num_test 100 \
   --gpu_ids 0,1 > rec/${FolderName}_test 
 
-FolderName=fr_adult_pix2pix_transfer_b6_25_1layer_fe
-python test_fr_aligned.py \
-  --dataroot ../InsightFace_Pytorch/data/facebank/noonan+normal/ \
-  --name $FolderName \
-  --dataset_mode unaligned \
-  --model $ModelName \
-  --netG resnet_4blocks \
-  --num_test 100 \
-  --gpu_ids 0,1 > rec/${FolderName}_test 
+# FolderName=fr_adult_pix2pix_transfer_b6_25_2layer
+# python test_fr_aligned.py \
+#   --dataroot ../InsightFace_Pytorch/data/facebank/noonan+normal/ \
+#   --name $FolderName \
+#   --dataset_mode unaligned \
+#   --model $ModelName \
+#   --netG resnet_4blocks \
+#   --num_test 100 \
+#   --gpu_ids 0,1 > rec/${FolderName}_test 
 
-FolderName=fr_adult_pix2pix_transfer_b6_25_1layer_pool5_DG
-python test_fr_aligned.py \
-  --dataroot ../InsightFace_Pytorch/data/facebank/noonan+normal/ \
-  --name $FolderName \
-  --dataset_mode unaligned \
-  --model $ModelName \
-  --netG resnet_4blocks \
-  --num_test 100 \
-  --gpu_ids 0,1 > rec/${FolderName}_test
+# FolderName=fr_adult_pix2pix_transfer_b6_25_1layer_fe
+# python test_fr_aligned.py \
+#   --dataroot ../InsightFace_Pytorch/data/facebank/noonan+normal/ \
+#   --name $FolderName \
+#   --dataset_mode unaligned \
+#   --model $ModelName \
+#   --netG resnet_4blocks \
+#   --num_test 100 \
+#   --gpu_ids 0,1 > rec/${FolderName}_test 
+
+# FolderName=fr_adult_pix2pix_transfer_b6_25_1layer_pool5_DG
+# python test_fr_aligned.py \
+#   --dataroot ../InsightFace_Pytorch/data/facebank/noonan+normal/ \
+#   --name $FolderName \
+#   --dataset_mode unaligned \
+#   --model $ModelName \
+#   --netG resnet_4blocks \
+#   --num_test 100 \
+#   --gpu_ids 0,1 > rec/${FolderName}_test
 
 
 # #################### test
