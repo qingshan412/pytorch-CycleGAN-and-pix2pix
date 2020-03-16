5bf54284d5193bf34a3df664c23934ada88eeb72
qingshan412 <qingshan412@hotmail.com>
2020-03-16 17:01:04 -0400

    Update run_fe.sh

--- run_fe.sh
+++ run_fe.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
-BatchSize=6
-ModelName=pix2pix_transfer #pix2pix #cycle_gan
-Iter=25 #25, 100, 500, 2000
-FolderName=fr_adult_${ModelName}_b${BatchSize}_${Iter}_1layer_pool5_DG_rr
-# fr_adult_${ModelName}_b${BatchSize}_${Iter}_1layer_pool5
 
-# cp -r ./checkpoints/fr_adult_basic_b6 ./checkpoints/${FolderName}
-#################### train on adults faces and then children faces
-python train_fr_aligned.py \
-  --dataroot ../InsightFace_Pytorch/data/facebank/resize+raw \
-  --continue_train \
-  --pool_size 5\
-  --name ${FolderName} \
-  --dataset_mode unaligned --model $ModelName --netG resnet_4blocks \
-  --batch_size $BatchSize --niter $Iter --niter_decay $Iter \
-  --display_id -1 --gpu_ids 0,1 \
-  --serial_batches > rec/${FolderName}_fe_rec
+#################### test
+FolderName=fr_lag_aug_pix2pix_transfer_b6_100_2layer
+python test_fr_aligned.py \
+  --dataroot ../InsightFace_Pytorch/data/facebank/noonan+normal/ \
+  --name $FolderName \
+  --dataset_mode unaligned \
+  --model $ModelName \
+  --netG resnet_4blocks \
+  --num_test 100 \
+  --gpu_ids 0,1 > rec/raw_${FolderName}_test 
+
+# BatchSize=6
+# ModelName=pix2pix_transfer #pix2pix #cycle_gan
+# Iter=25 #25, 100, 500, 2000
+# FolderName=fr_adult_${ModelName}_b${BatchSize}_${Iter}_1layer_pool5_DG_rr
+
+# # cp -r ./checkpoints/fr_adult_basic_b6 ./checkpoints/${FolderName}
+# #################### train on adults faces and then children faces
+# python train_fr_aligned.py \
+#   --dataroot ../InsightFace_Pytorch/data/facebank/resize+raw \
+#   --continue_train \
+#   --pool_size 5\
+#   --name ${FolderName} \
+#   --dataset_mode unaligned --model $ModelName --netG resnet_4blocks \
+#   --batch_size $BatchSize --niter $Iter --niter_decay $Iter \
+#   --display_id -1 --gpu_ids 0,1 \
+#   --serial_batches > rec/${FolderName}_fe_rec
