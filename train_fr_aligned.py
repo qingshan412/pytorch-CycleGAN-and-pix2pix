3cf47800d580587d8911720d2606e726a5a5e346
qingshan412 <qingshan412@hotmail.com>
2020-03-26 14:14:53 -0400

    Update train_fr_aligned.py

--- train_fr_aligned.py
+++ train_fr_aligned.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
 
             if total_steps % opt.display_freq == 0:
                 save_result = total_steps % opt.update_html_freq == 0
-                print('here:', data['A_paths'])
+                # print('here:', data['A_paths'])
                 visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
 
             if total_steps % opt.print_freq == 0:
